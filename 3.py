ab887b70bde981879710cb82044c5a1596417fb7
Nour-Alsous <156881089+Nour-Alsous@users.noreply.github.com>
2024-02-08 18:17:35 +0400

    Add files via upload
    This is the hexagon code for the project

---
+++ 3.py
@@ -0,0 +1,23 @@
+def hexagon(turta, hexa_color, border_color):
+    turta.color(border_color, hexa_color)
+    turta.begin_fill()
+
+    turta.forward(50)
+    turta.left(60)
+
+    turta.forward(50)
+    turta.left(60)
+
+    turta.forward(50)
+    turta.left(60)
+
+    turta.forward(50)
+    turta.left(60)
+
+    turta.forward(50)
+    turta.left(60)
+
+    turta.forward(50)
+    turta.left(60)
+
+    turta.end_fill()
