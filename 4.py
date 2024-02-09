26ca8bd3300a35e29208b15efad549521744de6a
youraveragedude <112537544+youraveragedude@users.noreply.github.com>
2024-02-09 10:28:11 +0400

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,18 @@
+def square(turta, square_color, border_color):
+    turta.color(border_color, square_color)
+    turta.begin_fill()
+
+    turta.forward(90)
+    turta.left(90)
+
+    turta.forward(90)
+    turta.left(90)
+
+    turta.forward(90)
+    turta.left(90)
+
+    turta.forward(90)
+    turta.left(90)
+
+    turta.end_fill()
+    
---
+++ moving.py.py
@@ -0,0 +1,4 @@
+def move(turta, m):
+    turta.penup()
+    turta.forward(m)
+    turta.pendown()
